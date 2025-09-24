ce2dc31cc27b34643420f41b8187e091d51b77c6
Dogukan-Ars <arss.dogukan@gmail.com>
2025-09-24 15:55:04 +0300

    try to fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,45 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import { defineConfig } from "vite";
+import { glob } from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import SortCss from "postcss-sort-media-queries";
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-rookies-project/',
-    root: 'src',
-    publicDir: '../public',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
+    base: "/goit-rookies-project/", // GitHub Pages için repo adı
+    root: "src", // Kaynak dosyalar src'den
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync("./*.html"), // Sadece src içindeki HTML'ler
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              return "vendor";
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
             }
-            return '[name].js';
+            return "[name].js";
           },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
             }
-            return 'assets/[name]-[hash][extname]';
+            return "assets/[name]-[hash][extname]"; // Hashed asset yolları
           },
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      outDir: "../dist", // Build çıktısı dist'e
+      emptyOutDir: true, // Eski build dosyalarını sil
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(["./**/*.html"]), // HTML değişikliklerini izle
       SortCss({
-        sort: 'mobile-first',
+        sort: "mobile-first",
       }),
     ],
   };
