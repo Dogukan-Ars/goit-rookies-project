3c2c261d69c9d0d1078d6610ada76eeebee116d4
Dogukan-Ars <arss.dogukan@gmail.com>
2025-09-24 16:42:24 +0300

    try to fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,49 @@
-import { defineConfig } from "vite";
-import { glob } from "glob";
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  base: "/goit-rookies-project/", // GitHub Pages için repo adını kullan
-  root: "src", // Root’u src olarak ayarla
-  build: {
-    rollupOptions: {
-      input: glob.sync("src/*.html"), // src içindeki HTML’leri giriş noktası yap
+export default defineConfig(({ command }) => {
+  return {
+    base: '/goit-rookies-project/',
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
 });
